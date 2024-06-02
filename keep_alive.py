25eb24f9d052ef214131704472af587b591395c9
Wild Life Studio <api@wild-life-studio.ir>
2024-06-03 01:16:15 +0330

    v3

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,15 @@
-from flask import Flask,render_template
+from flask import Flask
 from threading import Thread
+
 app = Flask(__name__)
+
 @app.route('/')
-def index():
-    return "Alive"
+def home():
+    return "I'm Alive"
+
 def run():
-  app.run(host='0.0.0.0',port=8080)
-def keep_alive():  
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
     t = Thread(target=run)
     t.start()
