d87abd5da2be35c05acc0e80d0ec549517669dde
Wild Life Studio <api@wild-life-studio.ir>
2024-07-02 11:46:44 +0330

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from threading import Thread
 from datetime import datetime
 from keep_alive import keep_alive
 # تنظیمات و متغیرهای مورد نیاز
-TOKEN = '7401177865:AAH1uD3QltIK7_a3ptOXP12UOWD8meVFP08'
+TOKEN = '7401177865:AAFthSG8nBPlFyIcjjnS3SIFR0vZROz5lNw'
 API_URL = f'http://tiamo.freehost.io/wl-ai-bot/ai.php?&msg='
 DATA_FILE = 'bot_data.json'
 ADMIN_USER_ID = 5694969786
